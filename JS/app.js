66d4039bd4245856baffd8b6af43b2733844368e
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-08-22 04:36:44 +0600

    added selected players

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,35 @@
-document.getElementById('select-add').addEventListener('click', function(){
-    // console.log('add clicked')
-    const 
-})
+const playerArray = [];
+console.log(playerArray)
+function displayPlayer(playerName){
+    const slectedPlayer = document.getElementById("player-name");
+    slectedPlayer.innerHTML = "";
+    console.log(slectedPlayer)
+
+    for(let i = 0; i < playerName.length; i++){
+    
+        const name = playerArray[i].playerName;
+    console.log(name)
+        const li = document.createElement("li");
+    
+        li.innerText = name;
+        slectedPlayer.appendChild(li);
+
+    }
+}
+
+
+function buyPlayer(element){
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    console.log(playerName);
+
+    const playerObj = {
+        playerName : playerName,
+       
+    }
+
+    playerArray.push(playerObj);
+
+    displayPlayer(playerArray);
+
+
+}
