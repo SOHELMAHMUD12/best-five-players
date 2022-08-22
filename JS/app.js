2e1255e26cf4cf5f62af0d21eeeaa8d0f5ebce2e
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-08-22 22:33:11 +0600

    added total cost

--- JS/app.js
+++ JS/app.js
@@ -1,45 +1,32 @@
 const playerArray = [];
-// console.log(playerArray)
+
 function displayPlayer(playerName){
     const slectedPlayer = document.getElementById("player-name");
     slectedPlayer.innerHTML = "";
-    console.log(slectedPlayer)
+    
 
     for(let i = 0; i < playerName.length; i++){
-
-        if (playerName.length > 5) {
+        if (playerName.length > 5){
             alert('player is not available');
             return;
-            }
-    
+        }
+            
         const name = playerArray[i].playerName;
-    console.log(name)
         const li = document.createElement("li");
-    
         li.innerText = name;
         slectedPlayer.appendChild(li);
-        
-
     }
 }
-
 
 function buyPlayer(element){
     const playerName = element.parentNode.parentNode.children[0].innerText;
-    console.log(playerName);
-
     const playerObj = {
         playerName : playerName,
-       
     }
 
     playerArray.push(playerObj);
-
     displayPlayer(playerArray);
-
-
 }
-
 
 // start calculator
 
@@ -47,11 +34,11 @@ document.getElementById('player-per').addEventListener('click', function(){
     const playerName = document.getElementById ('player-btn');
     const newPlayerNameString = playerName.value;
     const newPlayerName = parseFloat(newPlayerNameString);
-    console.log(newPlayerNameString);
-
-    const www = document.getElementById('ee')
-    const ccc = parseFloat(www.innerText);
-        www.innerText = newPlayerName
+    
+    const playerExpenses = document.getElementById('playerExpenses');
+    const playerExpensesAmaount = parseFloat(playerExpenses.innerText);
+    playerExpenses.innerText = newPlayerName * playerArray.length;
+        
 
 })
 
@@ -64,11 +51,13 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const newManagerNameString = managerName.value;
     const newManagerName = parseFloat(newManagerNameString);
 
-    const qqqqq =  document.getElementById('aa')
-    const ddd = parseFloat(qqqqq.innerText)
-    qqqqq.innerText = newManagerName + newCoachName;
+    const playerExpenses = document.getElementById('playerExpenses');
+    const playerExpensesAmaount = parseFloat(playerExpenses.innerText);
 
-    console.log(ddd);
+    const total =  document.getElementById('total');
+    const totalAmount = parseFloat(total.innerText);
+    total.innerText = newManagerName + newCoachName + playerExpensesAmaount ;
+
+    
 })
-
 
